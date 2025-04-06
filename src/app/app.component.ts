374881dcf9fb3bb2a68cecf33dc4eae87a78550a
caio2983 <caioluna2002@gmail.com>
2025-04-06 17:39:08 -0300

    remove header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { UploadFormComponent } from './upload-form/upload-form.component';
 import { DragDrop, DragDropModule } from '@angular/cdk/drag-drop';
 import { PaintingAreaComponent } from './painting-area/painting-area.component';
-import { HeaderComponent } from './header/header.component';
+
 import { ColorSelectComponent } from './color-select/color-select.component';
 import { MatExpansionModule } from '@angular/material/expansion';
 
@@ -14,7 +14,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
     UploadFormComponent,
     DragDropModule,
     PaintingAreaComponent,
-    HeaderComponent,
+
     ColorSelectComponent,
     MatExpansionModule,
   ],
