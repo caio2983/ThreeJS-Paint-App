b61e5493d132e7e2809b4e51a8391ba80d758c44
caio2983 <caioluna2002@gmail.com>
2025-04-06 20:25:07 -0300

    update orbit controls

--- src/app/three/three.component.ts
+++ src/app/three/three.component.ts
@@ -6,19 +6,21 @@ import {
   OnDestroy,
 } from '@angular/core';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 @Component({
   selector: 'app-three',
-  imports: [],
+  standalone: true,
   templateUrl: './three.component.html',
   styleUrl: './three.component.css',
 })
-export class ThreeComponent {
+export class ThreeComponent implements AfterViewInit, OnDestroy {
   @ViewChild('canvas') canvasRef!: ElementRef<HTMLCanvasElement>;
 
   private scene!: THREE.Scene;
   private camera!: THREE.PerspectiveCamera;
   private renderer!: THREE.WebGLRenderer;
+  private controls!: OrbitControls;
   private animationId!: number;
 
   ngAfterViewInit(): void {
@@ -35,6 +37,7 @@ export class ThreeComponent {
     const canvas = this.canvasRef.nativeElement;
 
     this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0xffffff);
 
     const aspect = window.innerWidth / window.innerHeight;
     this.camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
@@ -44,21 +47,27 @@ export class ThreeComponent {
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.renderer.setClearColor(0xffffff);
 
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.update();
+
     const light = new THREE.PointLight(0xffffff, 1);
     light.position.set(1, 1, 1);
     this.scene.add(light);
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+    this.scene.add(ambientLight);
 
     const geometry = new THREE.SphereGeometry(1, 32, 32);
     const material = new THREE.MeshStandardMaterial({ color: 'blue' });
     const sphere = new THREE.Mesh(geometry, material);
     this.scene.add(sphere);
-
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.3); // luz ambiente branca
-    this.scene.add(ambientLight);
   }
 
   private animate = () => {
     this.animationId = requestAnimationFrame(this.animate);
+
+    this.controls.update();
+
     this.renderer.render(this.scene, this.camera);
   };
 }
