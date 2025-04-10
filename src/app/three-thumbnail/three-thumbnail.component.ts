35e26adb097010cce6d32b685ae0c07f5ae87aca
caio2983 <caioluna2002@gmail.com>
2025-04-10 17:34:52 -0300

    update thumbnails

--- src/app/three-thumbnail/three-thumbnail.component.ts
+++ src/app/three-thumbnail/three-thumbnail.component.ts
@@ -17,7 +17,7 @@ import * as THREE from 'three';
 })
 export class ThreeThumbnailComponent implements AfterViewInit, OnDestroy {
   @ViewChild('canvas') canvasRef!: ElementRef<HTMLCanvasElement>;
-  @Input() geometryType: 'sphere' | 'plane' | 'box' | 'ring' | 'oct' = 'sphere';
+  @Input() geometryType!: string;
 
   private scene!: THREE.Scene;
   private camera!: THREE.PerspectiveCamera;
@@ -58,18 +58,20 @@ export class ThreeThumbnailComponent implements AfterViewInit, OnDestroy {
     light.position.set(5, 5, 5);
     this.scene.add(light);
 
-    const geometry = new THREE.SphereGeometry(1, 32, 32);
+    const geometry = this.createGeometry(this.geometryType);
     const material = new THREE.MeshStandardMaterial({ color: 0x0077ff });
     this.mesh = new THREE.Mesh(geometry, material);
     this.scene.add(this.mesh);
+    this.mesh.position.z -= 0.5;
 
     this.createGeometricGridBackground();
   }
 
   private createGeometricGridBackground(): void {
     const gridColor = new THREE.Color('midnightblue');
-    const gridHelper = new THREE.GridHelper(10, 20, gridColor, gridColor);
+    const gridHelper = new THREE.GridHelper(100, 100, gridColor, gridColor);
     gridHelper.rotation.x = Math.PI / 2;
+    gridHelper.position.z = -5;
     this.scene.add(gridHelper);
   }
 
@@ -77,8 +79,28 @@ export class ThreeThumbnailComponent implements AfterViewInit, OnDestroy {
     this.animationId = requestAnimationFrame(() => this.animate());
 
     this.mesh.rotation.y += 0.01;
-    this.mesh.rotation.x += 0.005;
+    this.mesh.rotation.x += 0.01;
 
     this.renderer.render(this.scene, this.camera);
+  }
+
+  createGeometry(type: string): THREE.BufferGeometry {
+    switch (type) {
+      case 'sphere':
+        return new THREE.SphereGeometry(1, 32, 32);
+      case 'plane':
+        return new THREE.PlaneGeometry(2, 2);
+      case 'box':
+        return new THREE.BoxGeometry(1, 1, 1);
+      case 'ring':
+        return new THREE.RingGeometry(0.8, 1, 32);
+      case 'oct':
+        return new THREE.OctahedronGeometry(1);
+      default:
+        console.warn(
+          `Geometria desconhecida: ${type}, usando Sphere por padrão`
+        );
+        return new THREE.SphereGeometry(1, 32, 32);
+    }
   }
 }
